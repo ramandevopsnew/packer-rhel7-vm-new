cb85eef06b0309ca82a6a0468fee8a4bda9ee301
ramandevopsnew <88483640+ramandevopsnew@users.noreply.github.com>
2021-08-06 10:44:15 +0530

    Update softwares.sh
    added GUI  config commands

--- Scripts/softwares.sh
+++ Scripts/softwares.sh
@@ -3,3 +3,5 @@ sudo subscription-manager attach --pool='8a85f9a07aaf843c017abea365cb2210'
 sudo yum update -y
 sudo yum install java-11-openjdk-devel -y 
 sudo yum install git -y
+sudo  yum groupinstall "Server with GUI" -y 
+sudo ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
