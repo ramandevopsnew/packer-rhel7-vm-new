cfec313de61a5bed21dd46276d1488e35381a2d2
ramandevopsnew <88483640+ramandevopsnew@users.noreply.github.com>
2021-08-06 11:08:58 +0530

    Delete Scripts directory
    No need it's already exist

--- Scripts/softwares.sh
+++
@@ -1,7 +0,0 @@
-sudo subscription-manager register --username='laxmangandhi' --password='Jul@2021' --name=packer-rhel7-$(date +%Y%m%d)-${RANDOM}
-sudo subscription-manager attach --pool='8a85f9a07aaf843c017abea365cb2210'
-sudo yum update -y
-sudo yum install java-11-openjdk-devel -y 
-sudo yum install git -y
-sudo  yum groupinstall "Server with GUI" -y 
-sudo ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
